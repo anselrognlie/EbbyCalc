c4ef4954b1a79574b1e79b2feadb8a9e159e0d67
Ansel Rognlie <ansel.rognlie@gmail.com>
2019-11-15 13:38:00 -0800

    Adds license copy at the top of all source files.
    Also adds appledoc to EWCCalculatorUserDefaultsData as a test.

--- HomeCalculator/EWCCalculatorUserDefaultsData.m
+++ HomeCalculator/EWCCalculatorUserDefaultsData.m
@@ -6,6 +6,19 @@
 //  Copyright © 2019 Ansel Rognlie. All rights reserved.
 //
 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #import "EWCCalculatorUserDefaultsData.h"
 
 static const char s_taxRateKey[] = "EWCCalculatorTaxRateKey";
@@ -13,6 +26,35 @@ static const char s_memoryKey[] = "EWCCalculatorMemoryKey";
 
 @implementation EWCCalculatorUserDefaultsData
 
+///------------------------------
+/// @name Property implementation
+///------------------------------
+
+- (NSDecimalNumber *)taxRate {
+  return [self getDecimalNumberForKey:@(s_taxRateKey) withDefault:[NSDecimalNumber zero]];
+}
+
+- (void)setTaxRate:(NSDecimalNumber *)value {
+  [self setDecimalNumber:value forKey:@(s_taxRateKey)];
+}
+
+- (NSDecimalNumber *)memory {
+  return [self getDecimalNumberForKey:@(s_memoryKey) withDefault:[NSDecimalNumber zero]];
+}
+
+- (void)setMemory:(NSDecimalNumber *)value {
+  [self setDecimalNumber:value forKey:@(s_memoryKey)];
+}
+
+///------------------------------
+/// @name Internal helper methods
+///------------------------------
+
+/**
+  Internal helper method that gets a configured `NSNumberFormatter` appropriate for reading or writing the persisted `NSDecimalNumber` values.
+
+  @return The configured formatter.
+ */
 - (NSNumberFormatter *)getFormatter {
   NSNumberFormatter *formatter = [NSNumberFormatter new];
 
@@ -24,6 +66,12 @@ static const char s_memoryKey[] = "EWCCalculatorMemoryKey";
   return formatter;
 }
 
+/**
+ Internal helper method that perists a supplied `NSDecimalNumber` value under the supplied name
+
+ @param value The `NSDecimalNumber` value to persist.
+ @param key The key under which to persist the value.
+*/
 - (void)setDecimalNumber:(NSDecimalNumber *)value forKey:(NSString *)key {
   NSNumberFormatter *formatter = [self getFormatter];
   [[NSUserDefaults standardUserDefaults]
@@ -31,6 +79,14 @@ static const char s_memoryKey[] = "EWCCalculatorMemoryKey";
     forKey:key];
 }
 
+/**
+ Internal helper method that retrieves an 'NSDecimalNumber' value with the supplied name
+
+ @param key The key used to retrieve the value.
+ @param defaultValue A value to return if the key cannot be located, or the stored value cannot otherwise be interpreted.
+
+ @return The value stored under the key, or the default value if an issue is encountered.
+*/
 - (NSDecimalNumber *)getDecimalNumberForKey:(NSString *)key
   withDefault:(NSDecimalNumber *)defaultValue {
 
@@ -47,22 +103,6 @@ static const char s_memoryKey[] = "EWCCalculatorMemoryKey";
   }
 
   return num;
-}
-
-- (NSDecimalNumber *)taxRate {
-  return [self getDecimalNumberForKey:@(s_taxRateKey) withDefault:[NSDecimalNumber zero]];
-}
-
-- (void)setTaxRate:(NSDecimalNumber *)value {
-  [self setDecimalNumber:value forKey:@(s_taxRateKey)];
-}
-
-- (NSDecimalNumber *)memory {
-  return [self getDecimalNumberForKey:@(s_memoryKey) withDefault:[NSDecimalNumber zero]];
-}
-
-- (void)setMemory:(NSDecimalNumber *)value {
-  [self setDecimalNumber:value forKey:@(s_memoryKey)];
 }
 
 @end
--- HomeCalculator/NSDecimalNumber+EWCMathCategory.m
+++ HomeCalculator/NSDecimalNumber+EWCMathCategory.m
@@ -6,8 +6,20 @@
 //  Copyright © 2019 Ansel Rognlie. All rights reserved.
 //
 
-#import "NSDecimalNumber+EWCMathCategory.h"
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#import "NSDecimalNumber+EWCMathCategory.h"
 
 static BOOL diffWithinDelta(NSDecimalNumber *n1, NSDecimalNumber *n2, NSDecimalNumber *delta) {
   NSDecimalNumber *diff = [n1 ewc_decimalNumberByAbsoluteDifferenceFrom:n2];
@@ -17,7 +29,6 @@ static BOOL diffWithinDelta(NSDecimalNumber *n1, NSDecimalNumber *n2, NSDecimalN
 
   return NO;
 }
-
 
 @implementation NSDecimalNumber (EWCMathCategory)
 

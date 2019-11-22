651987bb0b6d6d37665b313d54474a423c72e0b3
Ansel Rognlie <ansel.rognlie@gmail.com>
2019-11-21 16:31:54 -0800

    Removes extraneous debug logging from the clamping test.

--- HomeCalculatorTests/EWCMathTests.m
+++ HomeCalculatorTests/EWCMathTests.m
@@ -78,62 +78,52 @@ static NSDecimalNumber *makeDecimal(double d) {
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:0 isNegative:NO]; // 314
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertNil(clamp, @"%@ doesn't fit in 2 digits", num);
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:-1 isNegative:NO]; // 31.4
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
   str = [clamp stringValue];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertEqualObjects(@"31", str, @"should have dropped all decimals");
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:-2 isNegative:NO]; // 3.14
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
   str = [clamp stringValue];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertEqualObjects(@"3.1", str, @"should have one decimal digit");
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:-3 isNegative:NO]; // 0.314
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
   str = [clamp stringValue];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertEqualObjects(@"0.3", str, @"should have one decimal digit");
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:-4 isNegative:NO]; // 0.0314
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
   str = [clamp stringValue];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertEqualObjects(@"0", str, @"should clamp to zero");
 
   // negative
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:0 isNegative:YES]; // -314
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertNil(clamp, @"%@ doesn't fit in 2 digits", num);
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:-1 isNegative:YES]; // -31.4
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
   str = [clamp stringValue];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertEqualObjects(@"-31", str, @"should have dropped all decimals");
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:-2 isNegative:YES]; // -3.14
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
   str = [clamp stringValue];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertEqualObjects(@"-3.1", str, @"should have one decimal digit");
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:-3 isNegative:YES]; // -0.314
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
   str = [clamp stringValue];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertEqualObjects(@"-0.3", str, @"should have one decimal digit");
 
   num = [NSDecimalNumber decimalNumberWithMantissa:314 exponent:-4 isNegative:YES]; // -0.0314
   clamp = [num ewc_decimalNumberByRestrictingToDigits:2];
   str = [clamp stringValue];
-  NSLog(@"num: %@, clamp: %@", num, clamp);
   XCTAssertEqualObjects(@"0", str, @"should clamp to zero");
 
 }
